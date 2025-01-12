4498d0889b4430ee4006da9a49b7b1ae0e8cd9bd
Jarne Delarue <delarue.jarne@gmail.com>
2025-01-12 01:13:52 +0100

    add cloudflare beacon

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
   <div class="flex flex-col items-center md:mx-6">
     <Head>
       <title>Jarne Delarue</title>
+      <!-- Cloudflare Web Analytics -->
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "55b69d9a04c240d582fde78c5c00876c"}'
+      ></script>
+      <!-- End Cloudflare Web Analytics -->
     </Head>
     <Html lang="en"></Html>
     <div class="max-w-[55rem]">
